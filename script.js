ed5de6a915c1465b35b00bf3bfbeac5d2197ed62
Westorian <107351364+Westorian@users.noreply.github.com>
2022-06-23 21:17:02 +0300

    Second Home work

--- script.js
+++ script.js
@@ -6,16 +6,32 @@ const personalMovieDB = {
         count: numberOfFilms,
         movies: {},
         actors: {},
-        geners: [1],
+        geners: [],
         privat: false     
 };
 
-const lastFilm1 = prompt('Один из последних просмотренных фильмов?', ''),
-      ratingFilm1 = prompt('На сколько оцените его?', ''),
-      lastFilm2 = prompt('Один из последних просмотренных фильмов?', ''),
-      ratingFilm2 = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+        
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {   
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }    
+}
 
-personalMovieDB.movies[lastFilm1] = ratingFilm1;
-personalMovieDB.movies[lastFilm2] = ratingFilm2;
+if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Ви классический зритель");
+} else if (personalMovieDB.count >= 30 && personalMovieDB <= 50) {
+        console.log("Ви киноман");
+} else {
+        console.log("Произошла ошибка");
+}
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); 
+
